01c89af0097f9712fabc96dbd024627db78bbf24
Himmu <baburchauhan18@gmail.com>
2024-02-06 00:44:13 +0530

    second commit

--- app/Controllers/Http/DataStructuresController.ts
+++ app/Controllers/Http/DataStructuresController.ts
@@ -2,6 +2,8 @@
 
 export default class DataStructuresController {
     public async introduction({request, response}){
+        let d :any = request;
+        console.log(d)
         let data = [{question:'what is Dsa'},{question:'what is Algorithm'}]
         return response.json({
             'data':data,
